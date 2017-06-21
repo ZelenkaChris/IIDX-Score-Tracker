ee1b66be114e2a9f51325279c7f17f45bfba4238
Chris Zelenka <zelenka.chris@gmail.com>
2017-06-21 14:03:17 -0700

    Update index.php

--- index.php
+++ index.php
@@ -5,59 +5,56 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-		<meta charset="utf-8">
-		<meta http-equiv="Content-Language" content="ja" />
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="description" content="">
-		<meta name="author" content="">
-		<title>ChrisZ's IIDX Scores</title>
-	</head>
-	<style>
-		body {
-			background-color: #fcfcfc;
-		}
-	</style>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Language" content="ja" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>ChrisZ's IIDX Scores</title>
+  </head>
+  <style>
+    body {
+      background-color: #fcfcfc;
+    }
+  </style>
   <body>
     <div class="container">
-		<h1 style="margin-bottom: 10px;"align="center">ChrisZ's IIDX Scores</h1>
-		<h5 style="padding: 0px; margin: 5px;" align="center">IIDX ID: 5168-2956</h5>
-		<div>
-			<img style="display: block;margin-left: auto;margin-right: auto;" src="img/rank.png"></img>
-		</div>
-		<div align="center" class="row">
-			<div>
-				<h2>Select a song</h2>
-			</div>
-		</div>
-		<div class="row" align="center">
-			<div>
-				<select id="selector">
-					<?php
-						$i = 0;
-						$results = mysqli_query($conn, "SELECT * FROM songlist ORDER BY `songlist`.`title` ASC");
-						$num_rows = mysqli_num_rows($results);
-						while ($i < $num_rows) {
-							$row = $results->fetch_assoc();
-							$title = $row['title'];
-							$id = $row['ID'];
-							echo "<option value=\"data.php?id=$id&diff=\">$title</option>\n";
-							$i = $i + 1;
-						}
-					?>
-				</select>
-				<select id="diff_selector">
-					<option value="sph">SPH</option>
-					<option value="spa" selected="selected">SPA</option>
-				</select>
-			</div>			
-		</div>
-			<iframe id="iframe" style="width: 100%; height: 750px; border: 0; overflow-x: hidden;">
-			</iframe>		
-    </div> 
-
-    
+        <h1 style="margin-bottom: 10px;"align="center">ChrisZ's IIDX Scores</h1>
+        <h5 style="padding: 0px; margin: 5px;" align="center">IIDX ID: 5168-2956</h5>
+        <div>
+          <img style="display: block;margin-left: auto;margin-right: auto;" src="img/rank.png"></img>
+        </div>
+        <div align="center" class="row">
+        <div>
+          <h2>Select a song</h2>
+        </div>
+    </div>
+    <div class="row" align="center">
+      <div>
+        <select id="selector">
+          <?php
+            $i = 0;
+            $results = mysqli_query($conn, "SELECT * FROM songlist ORDER BY `songlist`.`title` ASC");
+            $num_rows = mysqli_num_rows($results);
+            while ($i < $num_rows) {
+              $row = $results->fetch_assoc();
+              $title = $row['title'];
+              $id = $row['ID'];
+              echo "<option value=\"data.php?id=$id&diff=\">$title</option>\n";
+              $i = $i + 1;
+            }
+          ?>
+        </select>
+        <select id="diff_selector">
+          <option value="sph">SPH</option>
+          <option value="spa" selected="selected">SPA</option>
+        </select>
+      </div>			
+    </div>
+    <iframe id="iframe" style="width: 100%; height: 750px; border: 0; overflow-x: hidden;"></iframe>		
+  </div> 
   </body>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
   <script>
 	// var select = document.createElement("select");
 	var iframe = document.getElementById("iframe");
